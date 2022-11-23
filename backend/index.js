07a981a35a38afe2e603bfb2fcf836cb010157ea
석환 김 <sh2.kim@saigeresearch.ai>
2022-11-24 08:17:26 +0900

    init commit

---
+++ backend/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const ExpressGraphQL = require("express-graphql");
+const schema = require("./graphql/post/post.js");
+const cors = require("cors");
+const app = express();
+
+app.use(cors());
+app.use("/graphql", ExpressGraphQL({ schema: schema.schema, graphiql: true }));
+
+app.listen(4000, () => {
+  console.log("GraphQL server running at http://localhost:4000.");
+});
